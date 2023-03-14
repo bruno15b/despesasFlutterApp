67c62bfb800663ba2aec58bd487ba00201132414
Bruno Machado <brunomachado3818@gmail.com>
2023-03-14 14:29:00 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Despesas Pessoais',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Despesas Pessoais'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text("Despesas Pessoais"),
+      ),
+      body: Column(
+
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                child:Text("Gráfico"),
+              ),
+            ),
+            Card(child: Text("Lista de Transações"))
+          ],
+        ),
+    );
+  }
+}
